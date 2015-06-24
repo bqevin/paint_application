66ebb08c50a8d9d77828c70f7bc138e0717725f4
Teddy Otieno <otienoasola@gmail.com>
2015-06-24 17:58:43 +0300

    Start of project

--- PaintAppProject/JS/canvas.js
+++ PaintAppProject/JS/canvas.js
@@ -1,5 +1,33 @@
 var canvas = document.getElementById('cvs');
 var drawingObj = canvas.getContext("2d");
-drawingObj.beginPath();
-drawingObj.arc(95,50,40,0,2 * Math.PI);
-drawingObj.stroke();
+
+//create the createDot function
+var radius = 4;
+
+var mouseDown = false;
+
+function mouseIsUP() {
+	mouseDown = false;
+}
+
+function mouseIsDown() {
+	mouseDown = true;
+}
+
+
+function createDot(act) {
+		if(mouseDown) {
+		drawingObj.beginPath();
+		drawingObj.arc(act.offsetX, act.offsetY, radius, 0, Math.PI * 2);
+		drawingObj.fill();	
+		}
+}
+
+//create testFunction 
+
+
+//create Event Listener on the Canvas Element 
+canvas.addEventListener("mousemove", createDot);
+canvas.addEventListener("mousedown", mouseIsDown);
+canvas.addEventListener("mouseup", mouseIsUP);
+
